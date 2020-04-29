b3a8687d29707346634d2107ecf9d4220021cfde
任俊家 <renjunjia1@163.com>
2020-04-29 15:37:20 +0800

    Initial commit

---
+++ SceneNettySocket/src/main/java/wiki/scene/socket/listener/MessageStateListener.java
@@ -0,0 +1,10 @@
+package wiki.scene.socket.listener;
+
+
+/**
+ * @author Created by LittleGreens on 2019/7/30
+ * <p>发送状态监听</p>
+ */
+public interface MessageStateListener {
+     void isSendSuccss(boolean isSuccess);
+}
---
+++ app/src/main/java/wiki/scene/mvp/scenenettysocketdemo/MainActivity.java
@@ -0,0 +1,14 @@
+package wiki.scene.mvp.scenenettysocketdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
